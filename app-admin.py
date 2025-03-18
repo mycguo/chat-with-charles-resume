89ba5f267d256f5255675bdbc7ac4e7b7498fe4c
charles <yinchao.guo@hpe.com>
2025-03-17 18:12:22 -0700

    remove admin part

---
+++ app-admin.py
@@ -0,0 +1,90 @@
+#https://www.youtube.com/watch?v=uus5eLz6smA
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GENAI_API_KEY"))
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf_doc in pdf_docs:
+        pdf = PdfReader(pdf_doc)
+        for page in pdf.pages:
+            text += page.extract_text()
+    return text
+
+
+def get_text_chunks(text):
+    splitter = RecursiveCharacterTextSplitter(chunk_size=5000, chunk_overlap=1000)
+    chunks = splitter.split_text(text)
+    return chunks   
+
+def get_vector_store(text_chunks):
+    embedding = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks,embedding=embedding)
+    vector_store.save_local("faiss_index")
+    return vector_store
+
+
+def get_prompt_template():
+    return PromptTemplate()
+
+def get_chat_chain():
+    prompt_template="""
+    Answer the questions based on my resume honestly
+
+    Context:\n {context} \n
+    Questions: \n {questions} \n
+
+    Answers:
+"""
+    model=ChatGoogleGenerativeAI(model="gemini-2.0-flash",temperature=0.3)
+    prompt=PromptTemplate(template=prompt_template, input_variabls=["context","questions"],output_variables=["answers"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    return chain
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector_store = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+    docs = vector_store.similarity_search(user_question)
+
+    chain = get_chat_chain()
+
+    response = chain({"input_documents": docs, "questions": user_question}, return_only_outputs=True)              
+
+    print(response)
+    st.write("Reply: ",response["output_text"])
+
+
+def main():
+    st.title("Resume Assistant")
+    st.header("chat with my resume")
+
+    user_question = st.text_input("Ask me a question")
+    if user_question:
+        user_input(user_question)
+
+    with st.sidebar:
+        pdf_docs = st.file_uploader("Upload your resume", type=["pdf"], accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing your resume..."):
+                if pdf_docs:
+                    text = get_pdf_text(pdf_docs)
+                    text_chunks = get_text_chunks(text)
+                    vector_store = get_vector_store(text_chunks)
+                    get_chat_chain()
+                    st.success("Resume processed successfully")
+
+if __name__ == "__main__":
+    main()
